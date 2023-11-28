a800ea0efacb6bf7a638a7c2ee95a88c8fbe5e15
Andres <quevedo0504@gmail.com>
2023-11-27 19:57:39 -0500

    Longitud de array

---
+++ script.js
@@ -0,0 +1,5 @@
+document.write("<h3>Elementos de un Arrays</h3>")
+
+const frutas = ["Manzana", " Pera", " Platano", " Fresa"]
+document.write("El Array de frutas es: " + frutas + "<br>")
+document.write("El Array de frutas tiene una longitud de:  " + frutas.length + "<br>")
